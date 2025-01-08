c765dffed09e3196fe14f510d1d5535543c4deee
newtratip <tharathip.chaweewongphan@gmail.com>
2025-01-09 00:51:22 +0700

    [ENH] Item code following reference type work order

---
+++ lpp_co/api.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2024, Ecosoft and contributors
+# For license information, please see license.txt
+
+import frappe
+
+
+@frappe.whitelist()
+def get_work_order_item(doctype, txt, searchfield, start, page_len, filters):
+    doc = frappe.get_doc(filters["reference_type"], filters["reference_name"])
+    return [(doc.production_item, )]
