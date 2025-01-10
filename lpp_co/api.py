ed1f4985d8fe479bec5d1e5b2d4d13dfd4a2a399
Saralrat <saralrats@ecosoft.co.th>
2025-01-10 15:32:28 +0700

    Add Quotation Form

--- lpp_co/api.py
+++ lpp_co/api.py
@@ -6,5 +6,5 @@ import frappe
 
 @frappe.whitelist()
 def get_work_order_item(doctype, txt, searchfield, start, page_len, filters):
-    doc = frappe.get_doc(filters["reference_type"], filters["reference_name"])
-    return [(doc.production_item, )]
+	doc = frappe.get_doc(filters["reference_type"], filters["reference_name"])
+	return [(doc.production_item,)]
