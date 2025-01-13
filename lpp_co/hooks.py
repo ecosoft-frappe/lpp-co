8b525a6d27819618c925a92a5440295ca4f4f631
newtratip <tharathip.chaweewongphan@gmail.com>
2025-01-13 12:11:17 +0700

    [ADD] jinja_methods.py

--- lpp_co/hooks.py
+++ lpp_co/hooks.py
@@ -85,6 +85,7 @@ doctype_js = {
 
 # add methods and filters to jinja environment
 jinja = {
+	"methods": "lpp_co.utils.jinja_methods",
 	"filters": "lpp_co.utils.jinja_filters",
 }
 
---
+++ lpp_co/utils/jinja_methods.py
@@ -0,0 +1,19 @@
+import frappe
+
+
+def get_company_info():
+	try:
+		# Get the Company Info data
+		return frappe.get_doc("Company Info")
+	except frappe.DoesNotExistError:
+		# Log error if needed and return None
+		# frappe.log_error("Company Info DocType does not exist", "get_company_info")
+		return None
+	except Exception:
+		# Handle any other unexpected exceptions
+		# frappe.log_error(f"Error retrieving Company Info: {e}", "get_company_info")
+		return None
+
+
+def chunk_list(lst: list, size):
+	return [lst[i : i + size] for i in range(0, len(lst), size)]
