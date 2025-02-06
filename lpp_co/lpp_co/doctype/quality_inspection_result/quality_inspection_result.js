067b1cc9ee1f76785dbd9d71ba9192d0256150e1
Kitti U <kittiu@gmail.com>
2025-02-07 00:13:02 +0700

    Change fieldtype of nominal, delta_plus, delta_minus, and reading to Data; add validation for reading input in Quality Inspection Result

--- lpp_co/lpp_co/doctype/quality_inspection_result/quality_inspection_result.js
+++ lpp_co/lpp_co/doctype/quality_inspection_result/quality_inspection_result.js
@@ -1,22 +1,36 @@
 // Copyright (c) 2025, Ecosoft and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Quality Inspection Result", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Quality Inspection Result", {
+    onload: function(frm) {
+		frm.set_df_property("readings", "cannot_add_rows", true);
+		frm.set_df_property("readings", "cannot_delete_rows", true);
+    }
+});
 
 // Event handler for the child table
 frappe.ui.form.on("Quality Inspection Result Reading", {
 	reading: function (frm, cdt, cdn) {
 		let row = locals[cdt][cdn];
-		let max = frm.doc.nominal + frm.doc.delta_plus;
-		let min = frm.doc.nominal - frm.doc.delta_minus;
-		if (min <= row.reading && row.reading <= max) {
-			frappe.model.set_value(cdt, cdn, "result", "Accepted");
-		} else {
-			frappe.model.set_value(cdt, cdn, "result", "Rejected");
+		if (allNumbers([frm.doc.nominal, frm.doc.delta_minus, frm.doc.delta_plus, row.reading])) {
+			let max = parseFloat(frm.doc.nominal) + parseFloat(frm.doc.delta_plus);
+			let min = parseFloat(frm.doc.nominal) - parseFloat(frm.doc.delta_minus);
+			let reading = parseFloat(row.reading);
+			if (min <= reading && reading <= max) {
+				frappe.model.set_value(cdt, cdn, "result", "Accepted");
+			} else {
+				frappe.model.set_value(cdt, cdn, "result", "Rejected");
+			}
 		}
 	}
 });
+
+function allNumbers(values) {
+    for (let i = 0; i < values.length; i++) {
+        let floatValue = parseFloat(values[i]);
+        if (isNaN(floatValue) || floatValue.toString() !== values[i].trim()) {
+            return false;
+        }
+    }
+    return true;
+}
