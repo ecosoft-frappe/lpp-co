a8da6632d2b5e8529d75cf0a81118d98a1c70356
newtratip <tharathip.chaweewongphan@gmail.com>
2025-02-14 12:51:38 +0700

    Skip check warehouse

--- lpp_co/__init__.py
+++ lpp_co/__init__.py
@@ -2,9 +2,12 @@ __version__ = "0.0.1"
 
 # Monkey patching, to make search widget display in next lines
 from .custom import search as lpp_search
+from .custom import utils as lpp_utils
 from frappe.desk import search as origin_search
+from erpnext.buying import utils as origin_utils
 
 origin_search.search_widget = lpp_search.search_widget
+origin_utils.validate_stock_item_warehouse = lpp_utils.validate_stock_item_warehouse
 
 # Monkey patching for mobile pattern to accept any string
 import re
