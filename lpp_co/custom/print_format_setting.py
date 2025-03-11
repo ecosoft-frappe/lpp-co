6ad309fc701151bf818074a2aa36e845ff15ca4d
newtratip <tharathip.chaweewongphan@gmail.com>
2025-03-11 19:15:24 +0700

    FIX Payment Receipt Form

--- lpp_co/custom/print_format_setting.py
+++ lpp_co/custom/print_format_setting.py
@@ -5,6 +5,7 @@ def set_print_format_as_disable():
 	print_formats = [
 		"Sales Order PD v2",
 		"Drop Shipping Format",
+		"Bank and Cash Payment Voucher",
 	]
 	for print_format in print_formats:
 		frappe.db.set_value("Print Format", print_format, "disabled", 1)
