f0cae6d8a8d6915fe4bb0db489b735f6c54fc5b7
Saralrat <saralrats@ecosoft.co.th>
2025-03-24 16:37:06 +0700

    disabled sales invoice and purchase invice print format

--- lpp_co/custom/print_format_setting.py
+++ lpp_co/custom/print_format_setting.py
@@ -6,6 +6,12 @@ def set_print_format_as_disable():
 		"Sales Order PD v2",
 		"Drop Shipping Format",
 		"Bank and Cash Payment Voucher",
+		"Purchase Auditing Voucher",
+		"Sales Auditing Voucher",
+		"Sales Invoice Return",
+		"Point of Sale",
+		"Sales Invoice PD Format v2",
+		"Sales Invoice Print",
 	]
 	for print_format in print_formats:
 		frappe.db.set_value("Print Format", print_format, "disabled", 1)
