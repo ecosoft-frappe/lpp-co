565684ed70f85225b0c2fae190809f5709a1cc94
newtratip <tharathip.chaweewongphan@gmail.com>
2025-03-24 21:26:44 +0700

    Disable form

--- lpp_co/custom/print_format_setting.py
+++ lpp_co/custom/print_format_setting.py
@@ -6,6 +6,7 @@ def set_print_format_as_disable():
 		"Sales Order PD v2",
 		"Drop Shipping Format",
 		"Bank and Cash Payment Voucher",
+		"Purchase Receipt Serial and Batch Bundle Print",
 	]
 	for print_format in print_formats:
 		frappe.db.set_value("Print Format", print_format, "disabled", 1)
