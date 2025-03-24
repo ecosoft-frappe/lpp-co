08563db0ab105b8a9668578c756ac24e0a642e6a
Saralrat <saralrats@ecosoft.co.th>
2025-03-24 16:48:26 +0700

    disabled journal entry  print format

--- lpp_co/custom/print_format_setting.py
+++ lpp_co/custom/print_format_setting.py
@@ -12,6 +12,11 @@ def set_print_format_as_disable():
 		"Point of Sale",
 		"Sales Invoice PD Format v2",
 		"Sales Invoice Print",
+		"General Expense Record",
+		"Credit Note",
+		"Journal Auditing Voucher",
+		"Payment Receipt Voucher",
+		"Cheque Printing Format",
 	]
 	for print_format in print_formats:
 		frappe.db.set_value("Print Format", print_format, "disabled", 1)
