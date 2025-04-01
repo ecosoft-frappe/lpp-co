b9db3cb02ec0a8fb60e0c38df4effaae48eb427f
Tharathip C. @ Ecosoft <tharathip.chaweewongphan@gmail.com>
2025-04-01 10:59:56 +0700

    Update print_format_setting.py

--- lpp_co/custom/print_format_setting.py
+++ lpp_co/custom/print_format_setting.py
@@ -19,6 +19,7 @@ def set_print_format_as_disable():
 		"Payment Receipt Voucher",
 		"Cheque Printing Format",
 		"RunCard",
+		"WO Original",
 	]
 	for print_format in print_formats:
 		frappe.db.set_value("Print Format", print_format, "disabled", 1)
