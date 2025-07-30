de32dd6fe53f859d949af868808c0a89c02c7b81
Saralrat <saralrats@ecosoft.co.th>
2025-07-30 13:35:55 +0700

    Use exact datetime from filter for to_time in Job Card Time Log query

--- lpp_co/lpp_co/report/รายงานการผลิต/รายงานการผลิต.js
+++ lpp_co/lpp_co/report/รายงานการผลิต/รายงานการผลิต.js
@@ -4,20 +4,6 @@
 frappe.query_reports["รายงานการผลิต"] = {
 	filters: [
 		{
-			label: __("From Posting Date"),
-			fieldname: "from_date",
-			fieldtype: "Datetime",
-			default: frappe.datetime.month_start(frappe.datetime.get_today()) + " 00:00:00",
-			reqd: 1,
-		},
-		{
-			label: __("To Posting Date"),
-			fieldname: "to_date",
-			fieldtype: "Datetime",
-			default: frappe.datetime.month_end(frappe.datetime.get_today()) + " 00:00:00",
-			reqd: 1,
-		},		
-		{
 			label: __("Status"),
 			fieldname: "status",
 			fieldtype: "Select",
@@ -58,6 +44,42 @@ frappe.query_reports["รายงานการผลิต"] = {
 			get_data: function (txt) {
 				return frappe.db.get_link_options("Item", txt);
 			},
+		},
+		{
+            label: __("From Posting Date"),
+            fieldname: "from_date",
+            fieldtype: "Datetime",
+            default: (() => {
+                const now = frappe.datetime.now_datetime();
+                const today_date = now.substr(0, 10);
+                const hour = parseInt(now.substr(11, 2));
+                let to_date_str = "";
+                if (hour >= 8) {
+                    to_date_str = today_date + " 08:00:00";
+                } else {
+                    to_date_str = frappe.datetime.add_days(today_date, -1) + " 08:00:00";
+                }
+                const from_date_only = frappe.datetime.add_days(to_date_str.substr(0, 10), -1);
+                return from_date_only + " 08:00:00";
+            })(),
+            reqd: 1,
+        },
+		{
+			label: __("To Posting Date"),
+			fieldname: "to_date",
+			fieldtype: "Datetime",
+			default: (() => {
+				const now = frappe.datetime.now_datetime(); 
+				const today_date = now.substr(0, 10);       
+				const hour = parseInt(now.substr(11, 2));
+				if (hour >= 8) {
+					return today_date + " 08:00:00";
+				} else {
+					const yesterday = frappe.datetime.add_days(today_date, -1);
+					return yesterday + " 08:00:00";
+				}
+			})(),
+			reqd: 1,
 		},
 	],
 };
